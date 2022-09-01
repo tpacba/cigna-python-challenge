a56578c43506341d2a31b243b0a969aa8b6695c1
tpacba <tpacba@live.com>
2022-09-01 01:21:13 -0400

    add json and yml

--- python/etl_script.py
+++ python/etl_script.py
@@ -1,6 +1,9 @@
 import csv
 from statistics import mean
 import sys
+import xml.etree.cElementTree as ET
+import json
+# import yaml
 
 
 def clean_name(str):
@@ -57,3 +60,30 @@ with open("python/data.csv") as csv_file:
     sys.stdout.write("The minimum value for all hostnames: " + str(min_val_all_host) + '\n')
     sys.stdout.write("The maximum value for all hostnames: " + str(max_val_all_host) + '\n')
     sys.stdout.write("The average value for all hostnames: " + str(avg_val_all_host) + '\n')
+
+    root = ET.Element("results")
+    ET.SubElement(root, "min_val_unique_host").text = str(min_val_unique_host)
+    ET.SubElement(root, "max_val_unique_host").text = str(min_val_unique_host)
+    ET.SubElement(root, "avg_val_unique_host").text = str(min_val_unique_host)
+    ET.SubElement(root, "min_val_all_host").text = str(min_val_all_host)
+    ET.SubElement(root, "max_val_all_host").text = str(max_val_all_host)
+    ET.SubElement(root, "avg_val_all_host").text = str(avg_val_all_host)
+    tree = ET.ElementTree(root)
+    tree.write("results_formatted.xml")
+
+    data = {
+            "min_val_unique_host": min_val_unique_host,
+            "max_val_unique_host": max_val_unique_host,
+            "avg_val_unique_host": avg_val_unique_host,
+            "min_val_all_host": min_val_all_host,
+            "max_val_all_host": max_val_all_host,
+            "avg_val_all_host": avg_val_all_host
+        }
+
+    with open("results_formatted.json", "w") as json_file:
+        json_file.write(json.dumps(data, indent=2))
+
+    # with open("results_formatted.yml", "w") as yml_file:
+    #     yaml.dump(data, yml_file)
+
+    
